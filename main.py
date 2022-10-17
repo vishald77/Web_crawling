91f27d9ea507acf0ddf80d9d41e9d7b7d98ca0dd
vishaldiwase <vishal.diwase@yahoo.com>
2022-10-17 21:26:02 +0530

    12-Oct-2022 URL fetcher for SG electronics

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import requests
 import re
 import sys
+from bs4 import BeautifulSoup
 # This is a sample Python script.
 
 
@@ -26,16 +27,83 @@ def product_url():
              'sec-gpc':'1',
              'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
              'x-requested-with':'XMLHttpRequest'}
-    payload={'query':'electronic company','Type':'PS','PageNumber':'1'}
 
-    data=requests.post(url,headers=headers,data=payload).text
+    payload = [{'query':'electronic company','Type':'PS','PageNumber':'1'},{'query': 'electronic company', 'Type': 'PS', 'PageNumber': '2'},{'query': 'electronic company', 'Type': 'PS', 'PageNumber': '3'}]
+
+    for p in payload:
+        print(p)
+        data=requests.post(url,headers=headers,data=p).text
+        output=json.loads(data)
+        # print(output)
+
+        for o in output['CompanyResultItems']:
+            print(o['LinkCompany'])
+
+def Crawl():
+
+    myList = ["/companies/interflux-singapore-pte-ltd","/companies/sm-system-control-pte-ltd","/companies/spm-refinery-pte-ltd","/companies/pca-technology-limited",
+    "/companies/esel-pte-ltd",
+    "/companies/dniv-international-pte-ltd",
+    "/companies/megachem-limited",
+    "/companies/tes-amm(singapore)-pte-ltd",
+    "/companies/micro-aire-care-pte-ltd",
+    "/companies/ltc-precision-pte-ltd",
+    "/companies/mitsubishi-electric-asia-pte-ltd",
+    "/companies/island-optical-systems-(s)-pte-ltd",
+    "/companies/d-mark-technologies-(s)-pte-ltd",
+    "/companies/globalink-electronics-(s)-pte-ltd",
+    "/companies/mccoy-pte-ltd",
+    "/companies/tassin-industrial-pte-ltd",
+    "/companies/mapletree-industrial-trust",
+    "/companies/seah-yong-heng-trading-pte-ltd",
+    "/companies/capitaland-limited",
+    "/companies/air-liquide-singapore-private-limited",
+    "/companies/ppt-vision-(s)-pte-ltd",
+    "/companies/alex-corporation-(s)-pte-ltd",
+    "/companies/marian-asia-pte-ltd",
+    "/companies/mieux-pte-ltd",
+    "/companies/vigor-precision-engineering-pte-ltd",
+    "/companies/add-plus-electronic-pte-ltd",
+    "/companies/iso-dynamique-microsystems-pte-ltd",
+    "/companies/reddot-production-pte-ltd",
+    "/companies/rokko-holdings-ltd",
+    "/companies/asmpt-smt-singapore-pte-ltd-"]
+
+    for l in myList:
+        main_url= 'https://www.sg-electronics.com/' + l
+        data = requests.get(main_url).content
+        soup=BeautifulSoup(data)
+
+        Comp_name=soup.find('div',{'class':'col-md-9 company-details'}).find('h3')
+        print(Comp_name)
+
+        crn = soup.find('p', {'class': 'company-reg'})
+        print(crn)
+
+        address = soup.find('div', {'class': 'col-md-7 company-contact'})
+        print(address)
+
+        phone = soup.find('div', {'class': 'valuephone'})
+        print(phone)
+
+        website = soup.find('div', {'class': 'valuewebsite'})
+        print(website)
+
+        description = soup.find('div', {'class': 'company-description'})
+        print(website)
 
 
-    output=json.loads(data)
-    # print(output)
 
-    for o in output['CompanyResultItems']:
-        print(o['LinkCompany'])
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -43,6 +111,6 @@ def product_url():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    product_url()
-
+    # product_url()
+    Crawl()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
